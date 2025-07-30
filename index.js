f9f94be2cf4968051f420d9f77cc72f5321c6dab
alternary <aapelisyren@proton.me>
2025-07-30 13:51:56 +0300

    uibuild

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 
 app.use(express.json())
 
-const cors = require('cors')
-app.use(cors())
+// const cors = require('cors')
+// app.use(cors())
 
 app.use(express.static('dist'))
 
